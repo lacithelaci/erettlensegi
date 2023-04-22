248bee11c2b2584677d85d477512381c6eb848d6
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2023-04-22 22:56:19 +0200

    Update szavak.py

--- szavak.py
+++ szavak.py
@@ -1,75 +1,78 @@
-lista = []
-f = open("szoveg.txt")
-for i in f:
-    i = i.strip()
-    lista.append(i)
-print("1. feladat")
-bekert_szo = input("Kérek egy szót")
-
-
-def mgh(a):
-    van = False
-    for i in "aeoiu":
-        if i in a:
-            van = True
-    return van
-
-
-if mgh(bekert_szo):
+maganhangzo = ["a", "e", "o", "u", "i"]
+szo = input("1. feladat Adjon meg egy szót:")
+van_benne = False
+for betu in szo:
+    if betu in maganhangzo:
+        van_benne = True
+if van_benne:
     print("Van benne magánhangzó.")
 else:
     print("Nincs benne magánhangzó.")
 print("2. feladat")
+f = open("szoveg.txt")
 leghosszabb = ""
-for i in sorted(lista, key=lambda i: len(i)):
-    leghosszabb = i
-print(f"A leghosszabb szó: {leghosszabb}\n{len(leghosszabb)} karakterből áll\n3. feladat")
+for szo in f:
+    szo = szo.strip()
+    if len(szo) > len(leghosszabb):
+        leghosszabb = szo
+print(
+    f"A leghosszabb szó a szoveg.txt állományban: {leghosszabb}. Ennyi karakterből áll: {len(leghosszabb)}\n3. feladat")
+lista = []
+f = open("szoveg.txt")
+for szo in f:
+    szo = szo.strip()
+    lista.append(szo)
 
 
-def melyik(a):
+def tobbmaganhagzo(szo):
+    maganhangzo = ["a", "e", "o", "u", "i"]
     mgh = 0
-    msh = 0
-    for i in a:
-        for y in "aeoiu":
-            if y == i:
-                mgh += 1
-    msh = len(a) - mgh
-    return mgh > msh
+    for betu in szo:
+        if betu in maganhangzo:
+            mgh += 1
+    return mgh / len(szo) > 0.5
 
 
-mgh = 0
-msh = 0
-for i in lista:
-    if melyik(i):
-        mgh += 1
-    else:
-        msh += 1
-print(f"{mgh}/{len(lista)} : {(mgh / len(lista)):.2f}% ")
+db = 0
+for szo in lista:
+    if tobbmaganhagzo(szo):
+        db += 1
+print(f"{db}/{len(lista)} : {(db / len(lista)):.2f}% ")
 print("4. feladat")
-otkaratkter = []
-for i in lista:
-    if len(i) == 5:
-        otkaratkter.append(i)
+
+
+def harombetu(szo):
+    return szo[1:4]
+
+
+otszavas = [szo for szo in lista if len(szo) == 5]
 szoreszlet = input("Kérek egy szórészletet")
-for i in otkaratkter:
-    if i[1:4] == szoreszlet:
-        print(i)
-print("5. feladat")
-
-
-def harom(a):
-    return a[1:4]
-
-
+for szo in otszavas:
+    if harombetu(szo) == szoreszlet:
+        print(szo, end=" ")
+print("\n5. feladat")
+szotar = {}
+letra = []
+for szo in otszavas:
+    szotar[harombetu(szo)] = szotar.get(harombetu(szo), 0) + 1
+for betuk, db in szotar.items():
+    for szo in otszavas:
+        if db > 1 and betuk == harombetu(szo):
+            letra.append(szo)
+f = open("letra.txt", "w")
 elozo = ""
 jelenlegi = ""
-f = open("letra.txt", "w", encoding="utf-8")
 db = 0
-for i in sorted(otkaratkter, key=lambda i: harom(i)):
-    jelenlegi = i
-    if db >= 1:
-        f.write(f"{elozo}\n")
-        if harom(jelenlegi) != harom(elozo):
+for szo in letra:
+    jelenlegi = szo
+    if db > 0:
+        if harombetu(jelenlegi) == harombetu(elozo):
+            f.write(f"{elozo}\n")
+        else:
+            f.write(f"{elozo}\n")
             f.write(f"\n")
-    db += 1
     elozo = jelenlegi
+    db += 1
+f.write(letra[-1])
+print(letra)
+
