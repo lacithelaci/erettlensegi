06ff2ace8fdacd09505b2fef680d3af6ee8a9cbd
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2023-04-22 22:54:43 +0200

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,71 +1,65 @@
-lista = []
-f = open("szotar.txt")
-for i in f:
-    i = i.strip()
-    lista.append(i)
-print('1. feladat')
-bekert_szo = input("Kérek egy szöveget")
-halmaz = set()
-for i in bekert_szo:
-    halmaz.add(i)
-print(f"{len(halmaz)} különböző karakter található a szövegben\n3. feladat")
-
-
-def sorbarendezett(a):
-    b = ""
-    for i in sorted(a):
-        b += i
-
-    return b
-
-
-print("Kész a fájlba írás")
-f = open("abc.txt", "w", encoding="utf-8")
-for i in lista:
-    f.write(f"{sorbarendezett(i)}\n")
+print("1. feladat")
+szoveg=input("Kérek egy szöveget)
+betui=set()
+for betu in szoveg:
+    betui.add(betu)
+print("Karakterszám:",len(betui))
+print("Karakterek:",*betui)
+lista=[]
+f=open("szotar.txt")
+for szo in f:
+    szo=szo.strip()
+    lista.append(szo)
+def sorba(szo):
+    szoveg=""
+    for betu in sorted(szo):
+        szoveg+=betu
+    return szoveg
+f=open("abc.txt","w")
+for szo in lista:
+    f.write(f"{sorba(szo)}\n")
 print("4. feladat")
-elso_szo = input("Első szó:")
-masodik_szo = input("Második szó:")
-if sorbarendezett(elso_szo) == sorbarendezett(masodik_szo):
+szo1=input("Kérek egy szöveget)
+szo2=input("Kérek egy másik szöveget)
+if sorba(szo1)==sorba(szo2):
     print("Anagramma")
 else:
     print("Nem anagramma")
 print("5. feladat")
-bekert_szo = input("Kérek egy szót")
-anagramma = []
-for i in lista:
-    if sorbarendezett(bekert_szo) == sorbarendezett(i):
-        anagramma.append(i)
-if len(anagramma) == 0:
+anagrammak=[]
+szo1=input("Kérek egy szöveget)
+for szo in lista:
+    if sorba(szo)==sorba(szo1):
+        anagrammak.append(szo)
+if len(anagrammak)==0:
     print("Nincs a szótárban anagramma")
 else:
-    print(f"{bekert_szo} anagrammái:")
-    for i in anagramma:
-        print(i)
+    print(*anagrammak)
 print("6. feladat")
-hossz = 0
-for i in sorted(lista, key=lambda i: len(i)):
-    hossz = len(i)
-print("A leghosszabb szavak: ")
-anagramma = []
-for i in lista:
-    if len(i) == hossz:
-        anagramma.append(i)
-for i in sorted(anagramma, key=lambda i: sorbarendezett(i)):
-    print(i)
-elozo = ""
-jelenlegi = ""
-db = 0
-f = open("rendezve.txt", "w")
-for i in sorted(lista, key=lambda i: (len(i), sorbarendezett(i))):
-
-    jelenlegi = i
-    if db != 0:
-        if sorbarendezett(jelenlegi) == sorbarendezett(elozo):
+maxhossz=0
+for szo in lista:
+    if maxhossz<len(szo):
+        maxhossz=len(szo)
+leghosszabbak=[szo for szo in lista if len(szo)==maxhossz]
+for szo in sorted(leghosszabbak,key=lambda szo:sorba(szo)):
+    print(szo)
+f=open("rendezve.txt","w")
+elozo=""
+jelenlegi=""
+db=0
+lista=sorted(lista,key=lambda szo:(len(szo),sorba(szo)))
+for szo in sorted(lista,key=lambda szo:(len(szo),sorba(szo))):
+    jelenlegi=szo
+    if db!=0:
+        if sorba(jelenlegi)==sorba(elozo):
             f.write(f"{elozo} ")
         else:
             f.write(f"{elozo}\n")
-        if len(jelenlegi) != len(elozo):
+        if len(jelenlegi)!=len(elozo):
             f.write("\n")
-    db += 1
-    elozo = jelenlegi
+    db+=1
+    elozo=jelenlegi
+if sorted(lista[-1])==sorted(lista[-2]):
+    f.write(f"{lista[-1]}")
+else:
+    f.write(f"\n{lista[-1]}")
