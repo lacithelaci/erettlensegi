c13a62dbf3d19a766a508ac9bdc03267586c6a7f
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2023-05-11 16:15:18 +0200

    Update tarsalgo.py

--- tarsalgo.py
+++ tarsalgo.py
@@ -1,74 +1,75 @@
 class Tarsalgo:
-    def __init__(self,ora,perc,az,kibe):
+    def __init__(self, ora, perc, az, kibe):
         self.ora = int(ora)
         self.perc = int(perc)
         self.az = int(az)
         self.kibe = kibe
+
     def __repr__(self):
         return f"{self.ora} {self.perc} {self.az} {self.kibe}"
-    def oopp(self):
-        return f"{str(self.ora)}:{str(self.perc)}"
-    def perc2(self):
-        return 60*self.ora+self.perc
-f=open("ajto.txt")
-lista=[]
+
+    def osszperc(self):
+        return self.ora * 60 + self.perc
+
+
+f = open("ajto.txt")
+lista = []
 for i in f:
-    i=i.strip().split()
+    i = i.strip().split()
     lista.append(Tarsalgo(*i))
-elso=lista[0]
-print(f"2. feladat\nAz első belépő: {elso.az}  ")
-utolso=[i.az for i in lista if i.kibe=="ki"]
-print(f"Az utolsó kilépő: {utolso[-1]} ")
-szotar= {}
-xd=open("athaladas.txt","w",encoding="UTF-8")
+print(f"2. feladat\nAz első belépő: {lista[0].az} ")
+ki = [i.az for i in lista if i.kibe == "ki"]
+print(f"Az utolsó kilépő: {ki[-1]} ")
+f = open("athaladas.txt", "w")
+szotar = {}
 for i in lista:
-    szotar[i.az]=szotar.get(i.az,0)+1
-for index,i in sorted(szotar.items()):
-    xd.write(f"{index} {i}\n")
-print("4. feladat")
-lista2=[]
-for index,i in szotar.items():
-    if i%2==1:
-        lista2.append(index)
-print("A végén a társalgóban voltak: ",*sorted(lista2))
-print("5. feladat ")
-bentlevok=0
-max=0
-jelenlegi=0
+    szotar[i.az] = szotar.get(i.az, 0) + 1
+for index, db in sorted(szotar.items()):
+    f.write(f"{index} {db}\n")
+print("4. feladat\nA végén a társalgóban voltak:", end=" ")
+for index, db in sorted(szotar.items()):
+    if db % 2 == 1:
+        print(index, end=" ")
+db = 0
+masz = 0
+ora_perc = None
 for i in lista:
-    if i.kibe=="be":
-        bentlevok+=1
-    if i.kibe=="ki":
-        bentlevok-=1
-    if bentlevok>max:
-        max=bentlevok
-        jelenlegi=i.oopp()
-print(f"Például {jelenlegi}-kor voltak a legtöbben a társalgóban.\n6.feladat ")
-a=int(input("Adja meg a személy azonosítóját!  "))
-print("7.feladat")
+    if i.kibe == "ki":
+        db -= 1
+    else:
+        db += 1
+    if db > masz:
+        masz = db
+        ora_perc = f"{i.ora}:{i.perc}"
+print(f"\n5. feladat\nPéldául {ora_perc}-kor voltak a legtöbben a társalgóban. ")
+print("6. feladat")
+az = int(input("Adja meg a személy azonosítóját! 22 "))
+print("7. feladat")
 for i in lista:
-    if a==i.az:
-        if i.kibe=="be":
-            print((i.oopp()),end="-")
-        if i.kibe=="ki":
-            print(f"{str(i.oopp())}")
-print("8.feladat")
-bentperc=0
-kintperc=0
-osszperc=0
-allapot="ki"
-for i in lista:
-    bentperc = 0
-    kintperc = 0
-    if a==i.az:
-        if i.kibe=="be":
-            bentperc=i.perc2()
-            allapot="ki"
-        if i.kibe=="ki":
-            kintperc=i.perc2()
-            allapot="be"
-    osszperc+=kintperc-bentperc
-if allapot=="ki":
-    print(f"A(z) {a}. személy összesen {osszperc + 15 * 60} percet volt bent, a megfigyelés végén a társalgóban volt. ")
+    if i.az == az and i.kibe == "be":
+        print(f"{i.ora}:{i.perc}-", end="")
+    elif i.az == az and i.kibe == "ki":
+        print(f"{i.ora}:{i.perc}")
+print("8. feladat")
+kollega = [i for i in lista if i.az == az]
+szum = 0
+bent_van = False
+kint_van = False
+bent_ido = None
+kint_ido = None
+for i in kollega:
+    if i.kibe == "be":
+        bent_ido = i.osszperc()
+        bent_van = True
+    else:
+        kint_ido = i.osszperc()
+        kint_van = True
+    if bent_van and kint_van:
+        szum += kint_ido - bent_ido
+        kint_van = False
+        bent_van = False
+if len(kollega) % 2 != 0:
+    szum += 15 * 60 - (kollega[-1].ora * 60 + kollega[-1].perc)
+    print(f"A(z) {az}. személy összesen {szum} percet volt bent, a megfigyelés végén a társalgóban volt. ")
 else:
-    print(f"A(z) {a}. személy összesen {osszperc} percet volt bent, a megfigyelés végén nem volt a társalgóban. ")
+    print(f"A(z) {az}. személy összesen {szum} percet volt bent, a megfigyelés végén nem volt a társalgóban. ")
