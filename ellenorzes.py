504903c8942d98cd42b69f9c1c27ffc49762207f
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2023-05-28 13:04:27 +0200

    Update ellenorzes.py

--- ellenorzes.py
+++ ellenorzes.py
@@ -1,85 +1,88 @@
-class Meres:
-    def __init__(self, rendszam, kora, kperc, kms, kems, vora, vperc, vms, vems):
+class Ellenorzes:
+    def __init__(self, rendszam, kora, kperc, kms, kezred, vora, vperc, vms, vezred):
         self.rendszam = rendszam
         self.kora = int(kora)
         self.kperc = int(kperc)
         self.kms = int(kms)
-        self.kems = int(kems)
+        self.kezred = int(kezred)
         self.vora = int(vora)
         self.vperc = int(vperc)
         self.vms = int(vms)
-        self.vems = int(vems)
+        self.vezred = int(vezred)
 
     def __repr__(self):
-        return f"{self.rendszam} {self.kora} {self.kperc} {self.kms} {self.kems} {self.vora} {self.vperc} {self.vms} {self.vems}"
+        
+        return f"{self.rendszam} {self.kora} {self.kperc} {self.kms} {self.kezred} {self.vora} {self.vperc} {self.vms} {self.vezred}"
 
-    def kmh(self):
-        osszms = (self.vora * 3600 + self.vperc * 60 + self.vms + self.vems / 1000) - (
-                self.kora * 3600 + self.kperc * 60 + self.kms + self.kems / 1000)
-        osszms = osszms / 3600
-        return int(10 / osszms)
+    def atlagkm(self):
+        
+        
+        return int(10 / (((self.vora * 3600 + self.vperc * 60 + self.vms + self.vezred / 1000) - (
+                self.kora * 3600 + self.kperc * 60 + self.kms + self.kezred / 1000)) / 3600))
 
     def vegms(self):
-        return self.vora * 3600 + self.vperc * 60 + self.vms + self.vems / 1000
+        
+        return self.vora * 3600 + self.vperc * 60 + self.vms + self.vezred / 1000
 
     def kezdms(self):
-        return self.kora * 3600 + self.kperc * 60 + self.kms + self.kems / 1000
+        
+        return self.kora * 3600 + self.kperc * 60 + self.kms + self.kezred / 1000
+
     def bunti(self):
-        if self.kmh()<=121:
-            return f"30 000 Ft"
-        elif  self.kmh()<=136:
-            return f"45 000 Ft"
-        elif self.kmh() <= 151:
-            return f"60 000 Ft"
+        
+        if self.atlagkm() <= 121:
+            return f"30000 Ft"
+        
+        elif self.atlagkm() <= 136:
+            return f"45000 Ft"
+        
+        elif self.atlagkm() <= 151:
+            return f"60000 Ft"
+        
         else:
-            return f"200 000 Ft"
+            return f"200000 Ft"
 
-lista = []
+
 f = open("meresek.txt")
+lista = []
 for i in f:
-    i = i.strip().split()
-    lista.append(Meres(*i))
+    
+    lista.append(Ellenorzes(*i.strip().split()))
+    
 print(f"2. feladat\nA mérés során {len(lista)} jármű adatait rögzítették.\n3. feladat")
-db = 0
-for i in lista:
-    if i.vora < 9:
-        db += 1
-print(f"9 óra előtt {db} jármű haladt el a végponti mérőnél.\n4. feladat")
-ora = 8
-perc = 20
-db = 0
-for i in lista:
-    if i.kora == ora and i.kperc == perc:
-        db += 1
-print(f"a. A kezdeti méréspontnál elhaladt járművek száma:", db)
-db = 0
-for i in lista:
-    if i.kora + i.kperc <= ora + perc and i.vperc + i.vora >= ora + perc:
-        db += 1
-print(f"b. A forgalomsűrűség: {db / 10}")
-print(f"5. feladat\nA legnagyobb sebességgel haladó jármű")
-masz = [i.kmh() for i in lista]
-a = max(masz)
-kezd = 0
-veg = 0
-for i in lista:
-    if a == i.kmh():
-        kezd = i.kezdms()
-        veg = i.vegms()
-        print(f"rendszáma: {i.rendszam}\nátlagsebessége: {i.kmh()} km/h ")
-        break
-db = 0
-for i in lista:
-    if i.kezdms() < kezd and i.vegms() > veg:
-        db += 1
-print(f"által lehagyott járművek száma: {db}\n6. feladat ")
-db = 0
-for i in lista:
-    if i.kmh() >= 90:
-        db += 1
-print(f"A járművek {(db / len(lista) * 100):.2f}%-a volt gyorshajtó.\nA fájl elkészült. ")
-f=open("buntetes.txt","w",encoding="utf-8")
-for i in lista:
-    if i.kmh()>=104:
-        f.write(f"{i.rendszam} {i.kmh()}\t{i.bunti()}\n")
 
+kilenc_elott = [i for i in lista if i.vora < 9]
+print(f"9 óra előtt {len(kilenc_elott)} jármű haladt el a végponti mérőnél.\n4. feladat\nAdjon meg egy óra és perc értéket! 8 20 ")
+
+ora = int(input())
+perc = int(input())
+
+megadott_idopont = [i for i in lista if i.kperc == perc and i.kora == ora]
+print(f"a. A kezdeti méréspontnál elhaladt járművek száma:", len(megadott_idopont))
+
+megadott_idopont2 = [i for i in lista if i.kperc <= perc <= i.vperc and i.kora <= ora <= i.vora]
+print(f"b. A forgalomsűrűség: {len(megadott_idopont2) / 10}")
+
+print(f"5. feladat")
+
+adatai = None
+km_orak = [i.atlagkm() for i in lista]
+
+for i in lista:
+    
+    if i.atlagkm() == max(km_orak):
+        adatai = i
+        print(f"rendszáma: {i.rendszam}\nátlagsebessége: {i.atlagkm()} km/h ")
+        break
+        
+lehagyottak = [i for i in lista if i.kezdms() < adatai.kezdms() and adatai.vegms() < i.vegms()]
+print(f"által lehagyott járművek száma: {len(lehagyottak)}\n6. feladat")
+
+kilencven_felett = [i for i in lista if i.atlagkm() >= 90]
+print(f"A járművek {len(kilencven_felett) / len(lista) * 100:.2f}%-a volt gyorshajtó. ")
+
+f=open("buntetes.txt","w")
+megbuntetett=[i for i in lista if i.atlagkm()>104]
+
+for i in megbuntetett:
+    f.write(f"{i.rendszam};{i.atlagkm()} km/h;{i.bunti()}\n")
