a8b6166bdd71d69761339623de57ce6c5dd61b7c
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2023-12-24 00:08:25 +0100

    Update lud.py

--- lud.py
+++ lud.py
@@ -1,98 +1,72 @@
 f = open("dobasok.txt")
-dobasok = []
 for i in f:
-    i = i.split()
-    for y in i:
-        dobasok.append(int(y))
+    dobasok = list(map(int, i.split()))
 f = open("osvenyek.txt")
 osvenyek = []
 for i in f:
     osvenyek.append(i.strip())
-print(f"2. feladat\nA dobások száma: {len(dobasok)}\nAz ösvények száma: {len(osvenyek)}")
-print(f"3. feladat")
-for i in sorted(osvenyek, key=lambda i: (len(i)), reverse=True):
-    leghosszabb = len(i)
-    break
-for index, i in enumerate(osvenyek):
-    if len(i) == leghosszabb:
-        print(f"Az egyik leghosszabb a(z) {index + 1}. ösvény, hossza: {leghosszabb}")
-
-print(f"4. feladat ")
-osvenyszam = int(input("Adja meg egy ösvény sorszámát! "))
-jatekosszam = int(input("Adja meg a játékosok számát! "))
+print(f"2. feladat\nA dobások száma: {len(dobasok)}\nAz ösvények száma: {len(osvenyek)}\n3. feladat")
+leghosszabb = ""
+szama = None
+for index, i in enumerate(osvenyek, 1):
+    if len(leghosszabb) < len(i):
+        leghosszabb = i
+        szama = index
+print(f"Az egyik leghosszabb a(z) {szama}. ösvény, hossza: {len(leghosszabb)}\n4. feladat")
+osveny = int(input("Adja meg egy ösvény sorszámát! 9 "))
+jatekos_szam = int(input("Adja meg a játékosok számát! 5 "))
 print("5. feladat")
-megadott_osveny = osvenyek[osvenyszam - 1]
+megadott_osveny = osvenyek[osveny - 1]
 szotar = {}
 for i in megadott_osveny:
     szotar[i] = szotar.get(i, 0) + 1
 for index, i in szotar.items():
     print(f"{index}: {i} darab ")
-f = open("kulonleges.txt", "w")
-for index, i in enumerate(megadott_osveny):
+# 6. feladat
+f = open("kulonleges.txt", "w", encoding="utf-8")
+for index, i in enumerate(megadott_osveny, 1):
     if i != "M":
-        f.write(f"{index + 1}\t{i}\n")
+        f.write(f"{index}\t{i}\n")
 print("7. feladat")
+hanyadik_mezon_vagyunk = [0 for i in range(jatekos_szam)]
+palyahossz = len(megadott_osveny)
 db = 0
-kezdet = db
-osszeg = -1
-lepesszam = 0
-lepesek = []
-while jatekosszam != db:
-    if osszeg >= len(megadott_osveny):
-        db += 1
-        kezdet = db
-        osszeg = -1
-        lepesek.append(lepesszam)
-        lepesszam = 0
-    osszeg += dobasok[kezdet]
-    kezdet += jatekosszam
-    lepesszam += 1
-leghamarabb_vegetert = min(lepesek)
-db = 0
-for i in lepesek:
+korok_szama = 0
+for i in dobasok:
+    hanyadik_mezon_vagyunk[db] = hanyadik_mezon_vagyunk[db] + i
     db += 1
-    if leghamarabb_vegetert == i:
-        print(f"A játék a(z) {leghamarabb_vegetert}. körben fejeződött be. A legtávolabb jutó(k) sorszáma: {db} ")
-print(f"8. feladat ")
-
+    if db == 5:
+        db = 0
+        korok_szama += 1
+    if max(hanyadik_mezon_vagyunk) >= palyahossz:
+        break
+for index, i in enumerate(hanyadik_mezon_vagyunk, 1):
+    if max(hanyadik_mezon_vagyunk) == i:
+        print(f"A játék a(z) {korok_szama}. körben fejeződött be. A legtávolabb jutó(k) sorszáma: {index} ")
+        break
+print("8. feladat")
+hanyadik_mezon_vagyunk = [0 for i in range(jatekos_szam)]
 db = 0
-kezdet = db
-osszeg = -1
-lepesszam = 0
-lepesek = []
-szamlalo = 0
-xedik = []
-kulonlista = []
-db3 = 0
-while jatekosszam != db:
-    osszeg += dobasok[kezdet]
-    if osszeg >= len(megadott_osveny):
-        db += 1
-        kezdet = db
-        osszeg = -1
-        lepesek.append(lepesszam)
-        lepesszam = 0
-        db3 = 0
-    if megadott_osveny[osszeg] == "V":
-        osszeg -= dobasok[kezdet]
-    elif megadott_osveny[osszeg] == "E":
-        osszeg += dobasok[kezdet]
-    kezdet += jatekosszam
-    lepesszam += 1
-    if osszeg > 0:
-        db3 += 1
-    if db3 == 55:
-        kulonlista.append(osszeg)
-
-db = 0
-ugyanaz = [3, 1, 4]
-print("Nyertes(ek):", end=" ")
-leggyorsabbak = min(lepesek)
-for i in lepesek:
+for i in dobasok:
+    hanyadik_mezon_vagyunk[db] = hanyadik_mezon_vagyunk[db] + i
+    if max(hanyadik_mezon_vagyunk) < palyahossz:
+        if megadott_osveny[hanyadik_mezon_vagyunk[db] - 1] == "E":
+            hanyadik_mezon_vagyunk[db] = hanyadik_mezon_vagyunk[db] + i
+        elif megadott_osveny[hanyadik_mezon_vagyunk[db] - 1] == "V":
+            hanyadik_mezon_vagyunk[db] = hanyadik_mezon_vagyunk[db] - i
     db += 1
-    if leggyorsabbak == i:
-        print(db, end=" ")
-osszeg = 0
-print("A többiek pozíciója: ")
-for i in range(1, 4):
-    print(f"{i}. játékos, {kulonlista[i - 1] + ugyanaz[i - 1]}. mező")
+    if db == 5:
+        db = 0
+    if max(hanyadik_mezon_vagyunk) >= palyahossz and 0 == db:
+        break
+gyoztesek = []
+vesztesek = []
+for index, i in enumerate(hanyadik_mezon_vagyunk, 1):
+    if max(hanyadik_mezon_vagyunk) == i:
+        gyoztesek.append(index)
+    else:
+        vesztesek.append(index)
+print(f"Nyertes(ek):", *gyoztesek)
+print(f"A többiek pozíciója: ")
+for i in vesztesek:
+    print(f"{i}. játékos, {hanyadik_mezon_vagyunk[i - 1]}. mező")
